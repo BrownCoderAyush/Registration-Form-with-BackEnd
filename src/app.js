a9de68f3db23541b15ca24e61d360c7f70ee4dd7
Ayush <ayushchopra0@gmail.com>
2021-10-24 23:55:52 +0530

    Created a backend with registration form

---
+++ src/app.js
@@ -0,0 +1,125 @@
+require("dotenv").config();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const { read } = require("fs");
+const hbs = require("hbs");
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 8000;
+const app = express();
+require("../src/db/conn");
+const path = require("path");
+const Register = require("../src/models/registers");
+
+const static_path = path.join(__dirname, "../public");
+
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+// for customized views directory 
+app.set("views", path.join(__dirname, "../templete/views"));
+
+// for registering the use of partials 
+hbs.registerPartials(path.join(__dirname, "../templete/partials"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// console.log(process.env.SECRET_KEY);
+// console.log(typeof(process.env.SECRET_KEY));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+app.post("/login", async (req, res) => {
+    try {
+        // console.log(email);
+        // console.log(password);
+        // console.log(Data);
+        // console.log(Data.pass word);
+        // console.log(password);
+        const email = req.body.email;
+        const password = req.body.password;
+      
+        
+        const Data = await Register.findOne({ email: email });
+        // console.log("hashed password is~~~~~~~");
+        // console.log(password);
+        // console.log( await bcrypt.hash(password,10)); 
+        // console.log("Database password is~~~~~~~");
+        // console.log(Data.password);
+        // const isMatched = await bcrypt.compare(password, Data.password);
+        const token = await Data.generateAuthToken();
+        console.log(token);
+        // console.log(isMatched);
+        if (password==Data.password) {
+            res.render("index");
+        }
+        else {
+            res.send("not valid combination");
+        }
+    } catch (error) {
+        res.status(400).send("invalid");
+    }
+})
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+app.post("/register", async (req, res) => {
+
+    try {
+        const password = req.body.password;
+        const confirmPassword = req.body.confirmPassword;
+        if (password === confirmPassword) {
+            const registerEmployee = new Register({
+                email: req.body.email,
+                password: req.body.password,
+                name: req.body.name,
+                gender: req.body.gender,
+                confirmPassword: req.body.confirmPassword,
+                phone: req.body.Phone,
+            });
+        // console.log(`the success part is ~~~ ${registerEmployee}`);
+        // this is going to run the generateAuthToken function present in registerEmployee
+
+        const token = await registerEmployee.generateAuthToken();
+        console.log(token);
+
+            const register = await registerEmployee.save();
+            res.render("index");
+        } else {
+            res.send("Password is not matching");
+        }
+
+
+    } catch (error) {
+        res.send(error);
+    }
+
+    // console.log(req.body.email);
+    // console.log(req.body.password);
+    // console.log(req.body);
+    // res.send(req.body);
+})
+// const createToken = async ()=>{
+//     const token = await jwt.sign({_id:1234},"ayushIzOpzzzzzzzzzzzzzzzzzzcccccccccccccccccccccccccccccccccc",{
+//         expiresIn:"2 minutes"
+//     });
+//     console.log(token);
+//     const isVerify = await jwt.verify(token,"ayushIzOpzzzzzzzzzzzzzzzzzzcccccccccccccccccccccccccccccccccc");
+//     console.log(isVerify);
+// }
+// createToken();
+app.listen(port, () => {
+    console.log(`server running at port no ${port}`);
+})
+
+// const Bcrypt = async (password)=>{
+//     const passwordHash =await bcrypt.hash(password,10);
+//     console.log(passwordHash);
+//     const passwordSrch = await bcrypt.compare(password,passwordHash);
+//     console.log(passwordSrch);
+// }
+// Bcrypt("ayush");
---
+++ src/models/registers.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const employeeSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        // this will create problem in this.confirmPassword = undefined area
+        unique:true
+    }
+    ,
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+}) 
+employeeSchema.methods.generateAuthToken = async function(){
+    try {
+        // this is going to console the id of the object created using new Register 
+        console.log(this._id);
+
+        // this is going to take the id as payload and change the result into a jwt token 
+        const token = await jwt.sign({_id:this._id},process.env.SECRET_KEY);
+        // this will help in filling the genetated tooken in place of token field in schema 
+        this.tokens = this.tokens.concat({token:token});
+        // this needs to be called as it is going to save the token in db 
+        await this.save();
+        // console.log(token);
+        
+        return token;
+    } catch (err) {
+        res.send("the error path "+ err);
+        console.log(err);
+    }
+}
+
+// cant use fat arrow function
+// employeeSchema.pre("save", async function (next){  
+//     // like when we want to update the information then if other than password if we change another field then this hashing will not be done
+//     // if(this.isModified("password")){
+//    console.log(`the password is - ${this.password}`);
+//    this.password = await bcrypt.hash(this.password,8);
+// //    this.confirmPassword = await bcrypt.hash(this.confirmPassword,10);
+// //    this.confirmPassword = undefined;
+//    console.log(`the password after is - ${this.password}`);
+//     // }
+//    next();
+
+// })
+
+const Register = new mongoose.model("Register",employeeSchema);
+module.exports=Register;
